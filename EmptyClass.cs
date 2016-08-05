36bbbb69dfc649680d7f7d8ca35d6824f3c9078c
JiaxinSu <jiaxin.su@idt.net>
2016-08-05 16:15:04 -0400

    "adding code to open browser"`

--- EmptyClass.cs
+++ EmptyClass.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 namespace MTUdemo
 {
+	[TestFixture]
 	public class EmptyClass
 	{
-		public EmptyClass()
+		private IWebDriver driver;
+		string url = "http://symbiote-app.herokuapp.com/";
+
+		[SetUp]
+		public void TestSetup()
 		{
+			driver = new FirefoxDriver();
+			driver.Navigate().GoToUrl(url);
 		}
 
-		public static void Main()
+		[TearDown]
+		public void Cleanup()
 		{
-			Console.WriteLine("Testing!");
-			Console.ReadLine();
-			Console.WriteLine("ughas;gkjas;dflkjas;dfljasd;fljasd;lfkjasd;fkjfs");
+			driver.Quit();
+		}
+
+		[Test]
+		public void LoginAsAdmin()
+		{
+			driver.FindElement(By.Id("open")).Click();
+			driver.FindElement(By.Id("username")).SendKeys("admin");
+			driver.FindElement(By.Id("password")).SendKeys("admin");
+			driver.FindElement(By.Id("login-button")).Click();
 		}
 	}
 }
