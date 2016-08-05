cef9a419a87363d5cc567f337f68e9d4f58e0095
Jiaxin Su <js4722@columbia.edu>
2016-08-05 14:30:55 -0400

    Initial check-in of module MTUdemo

---
+++ EmptyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MTUdemo
+{
+	public class EmptyClass
+	{
+		public EmptyClass()
+		{
+		}
+
+		public static void Main()
+		{
+			Console.WriteLine("Testing!");
+			Console.ReadLine();
+		}
+	}
+}
+
